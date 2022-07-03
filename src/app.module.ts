1e132bef794c2a4b1810e9e7842374f799a348ef
Danil Panarin <danilapanarin96@gmail.com>
2022-07-03 13:03:19 +0600

    fix-app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { CoursePageModule } from './course-page/course-page.module';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
+
 
 @Module({
-  imports: [AuthModule, CoursePageModule, ProductModule, ReviewModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.model.ts
+++
@@ -1,4 +0,0 @@
-export class AuthModel {
-	email: string;
-	passwordHsh: string;
-}
