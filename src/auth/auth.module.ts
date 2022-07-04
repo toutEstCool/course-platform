65e063a7a3c1885daad147c57642302f522b5034
Danil Panarin <danilapanarin96@gmail.com>
2022-07-04 13:16:36 +0600

    tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { TypegooseModule } from 'nestjs-typegoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJWTConfig } from 'src/config/jwt.config'
+
+import { AuthService } from './auth.service'
 import { UserModel } from 'src/user/user.model'
-import { ConfigModule } from '@nestjs/config'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
+	controllers: [AuthController],
 	imports: [
 		TypegooseModule.forFeature([
 			{
@@ -16,8 +21,12 @@ import { ConfigModule } from '@nestjs/config'
 			},
 		]),
 		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
 	],
-	providers: [AuthService],
-	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,15 +9,22 @@ import { hash, genSalt, compare } from 'bcryptjs'
 
 import { UserModel } from 'src/user/user.model'
 import { AuthDto } from './dto/auth.dto'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
 	constructor(
-		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
 	) {}
 
 	async login(dto: AuthDto) {
-		return this.validateUser(dto)
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokenPair(String(user._id))
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
 	}
 
 	async register(dto: AuthDto) {
@@ -33,7 +40,13 @@ export class AuthService {
 			email: dto.email,
 			password: await hash(dto.password, salt),
 		})
-		return newUser.save()
+
+		const user = await newUser.save()
+		const tokens = await this.issueTokenPair(String(user._id))
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
 	}
 
 	async validateUser(dto: AuthDto): Promise<UserModel> {
@@ -42,9 +55,29 @@ export class AuthService {
 
 		const isValidPassword = await compare(dto.password, user.password)
 		if (!isValidPassword)
-			throw new UnauthorizedException(
-				'Не верный логин или пароль. Пароль именно'
-			)
+			throw new UnauthorizedException('Не верный логин или пароль.')
 		return user
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId }
+
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '10d',
+		})
+
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '1h',
+		})
+
+		return { refreshToken, accessToken }
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		}
 	}
 }
