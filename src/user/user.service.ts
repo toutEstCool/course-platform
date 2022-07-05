79de53a532b21ae3995c098b099f358eacdf60f7
Danil Panarin <danilapanarin96@gmail.com>
2022-07-05 14:05:35 +0600

    profile(del, up, count)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
+import { genSalt, hash } from 'bcryptjs'
 import { InjectModel } from 'nestjs-typegoose'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 import { UserModel } from './user.model'
 
@@ -15,5 +17,51 @@ export class UserService {
 		if (!user) throw new NotFoundException('Не верный логин или пароль.')
 
 		return user
+	}
+
+	async updateProfile(_id: string, dto: UpdateUserDto) {
+		const user = await this.byId(_id)
+		const isSameUser = await this.UserModel.findOne({ email: dto.email })
+
+		if (isSameUser && String(_id) !== String(isSameUser._id))
+			throw new NotFoundException('Данный email уже используется.')
+
+		if (dto.password) {
+			const salt = await genSalt(10)
+			user.password = await hash(dto.password, salt)
+		}
+		user.email = dto.email
+
+		if (dto.isAdmin || dto.isAdmin === false) user.isAdmin = dto.isAdmin
+
+		await user.save()
+
+		return
+	}
+
+	async getCount() {
+		return this.UserModel.find().count().exec()
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {}
+		if (searchTerm)
+			options = {
+				$or: [
+					{
+						email: new RegExp(searchTerm, 'i'),
+					},
+				],
+			}
+
+		return this.UserModel.find(options)
+			.select('-password -updatedAt -__v')
+			.sort({
+				createdAt: 'desc',
+			})
+	}
+
+	async delete(id: string) {
+		return this.UserModel.findByIdAndDelete(id).exec()
 	}
 }
