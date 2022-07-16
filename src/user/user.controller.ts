546d7f8bc6fccbeb9e11c7f136e3b782c30dddaa
Danil Panarin <danilapanarin96@gmail.com>
2022-07-16 21:55:25 +0600

    favorites

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,6 +16,8 @@ import { UserService } from './user.service'
 import { User } from './decorators/user.decorator'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { IdValidationPipe } from 'src/pipes/id.validation.pipe'
+import { Types } from 'mongoose'
+import { UserModel } from './user.model'
 
 @Controller('users')
 export class UserController {
@@ -33,6 +35,22 @@ export class UserController {
 	@Auth()
 	async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
 		return this.userService.updateProfile(_id, dto)
+	}
+
+	@Get('profile/favorites')
+	@Auth()
+	async getFavorites(@User('_id') _id: Types.ObjectId) {
+		return this.userService.getFavoriteMovies(_id)
+	}
+
+	@Put('profile/favorites')
+	@HttpCode(200)
+	@Auth()
+	async toggleFavorite(
+		@Body('movieId', IdValidationPipe) movieId: Types.ObjectId,
+		@User() user: UserModel
+	) {
+		return this.userService.toggleFavorite(movieId, user)
 	}
 
 	@Get('count')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { genSalt, hash } from 'bcryptjs'
+import { Types } from 'mongoose'
 import { InjectModel } from 'nestjs-typegoose'
 import { UpdateUserDto } from './dto/update-user.dto'
 
@@ -63,5 +64,26 @@ export class UserService {
 
 	async delete(id: string) {
 		return this.UserModel.findByIdAndDelete(id).exec()
+	}
+
+	async toggleFavorite(movieId: Types.ObjectId, user: UserModel) {
+		const { _id, favorites } = user
+		await this.UserModel.findOneAndUpdate(_id, {
+			favorites: favorites.includes(movieId)
+				? favorites.filter((id) => String(id) !== String(movieId))
+				: [...favorites, movieId],
+		})
+	}
+
+	async getFavoriteMovies(_id: Types.ObjectId) {
+		return this.UserModel.findById(_id, 'favorites')
+			.populate({
+				path: 'favorites',
+				populate: {
+					path: 'genres',
+				},
+			})
+			.exec()
+			.then((data) => data.favorites)
 	}
 }
