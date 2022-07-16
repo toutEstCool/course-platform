db5f685befb2d833bcde484d96d91ba364e85278
Danil Panarin <danilapanarin96@gmail.com>
2022-07-16 13:14:32 +0600

    movie-(model)

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { UserModule } from './user/user.module'
 import { GenreModule } from './genre/genre.module';
 import { FileModule } from './file/file.module';
 import { MentorModule } from './mentor/mentor.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
 	imports: [
@@ -23,6 +24,7 @@ import { MentorModule } from './mentor/mentor.module';
 		GenreModule,
 		FileModule,
 		MentorModule,
+		MovieModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
---
+++ src/movie/movie.model.ts
@@ -0,0 +1,55 @@
+import { prop, Ref } from '@typegoose/typegoose'
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import { GenreModel } from 'src/genre/genre.model'
+import { MentorModel } from 'src/mentor/mentor.model'
+
+export interface MovieModel extends Base {}
+
+export class Parameters {
+	@prop()
+	year: number
+
+	@prop()
+	duration: number
+
+	@prop()
+	country: string
+}
+
+export class MovieModel extends TimeStamps {
+	@prop()
+	poster: string
+
+	@prop()
+	bigPoster: string
+
+	@prop()
+	title: string
+
+	@prop()
+	description: string
+
+	@prop({ unique: true })
+	slug: string
+
+	@prop()
+	parameters?: Parameters
+
+	@prop({ default: 4.0 })
+	rating?: number
+
+	@prop()
+	videoUrl: string
+
+	@prop({ default: 0 })
+	countOpened?: number
+
+	@prop({ ref: () => GenreModel })
+	genres: Ref<GenreModel>[]
+
+	@prop({ ref: () => MentorModel })
+	mentors: Ref<MentorModel>[]
+
+	@prop({ default: false })
+	isSendTelegram?: boolean
+}
